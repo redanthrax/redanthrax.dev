c1517b35fa34801ca34dca6a9ff6dcd5a2c7db7a
redanthrax <redanthraxx@gmail.com>
2022-06-26 16:01:21 -0700

    removed tags breaking build

--- app/.vitepress/theme/index.ts
+++ app/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import Layout from './Layout.vue'
 import DefaultTheme from 'vitepress/theme'
 import Posts from './components/Posts.vue'
-import Tags from './components/Tags.vue'
 import PostCard from './components/PostCard.vue'
 import './custom.css'
 
@@ -10,7 +9,6 @@ export default {
     Layout,
     enhanceApp({ app, router, siteData }) {
         app.component('Posts', Posts)
-        app.component('Tags', Tags)
         app.component('PostCard', PostCard)
     }
 }
